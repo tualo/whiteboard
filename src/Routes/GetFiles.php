04b16028957d1cddca20a159f7f813ad1b511e8a
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:32:21 +0100

    Version bump to 1.0.3

--- src/Routes/GetFiles.php
+++ src/Routes/GetFiles.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tualo\Office\HBK\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
@@ -7,61 +8,62 @@ use Tualo\Office\Basic\IRoute;
 use phpseclib3\Net\SFTP;
 use Tualo\Office\HBK\HlsHelper;
 
-class GetFiles implements IRoute{
-    public static function register(){
+class GetFiles extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
 
 
-        BasicRoute::add('/hbk/getfiles',function($matches){
+        BasicRoute::add('/hbk/getfiles', function ($matches) {
             $db = App::get('session')->getDB();
             App::contenttype('application/json');
-            try{
-                App::set('hlsJobDir',HLS_JOB_DIR);
-                
-                $server = $db->singleRow('select * from hbk_sftp_import limit 1 ',$matches);
+            try {
+                App::set('hlsJobDir', HLS_JOB_DIR);
+
+                $server = $db->singleRow('select * from hbk_sftp_import limit 1 ', $matches);
                 if ($server === false) throw new \Exception('No SFTP configured');
-        
+
                 $sftp_server = $server['server'];
                 $sftp_port = 22;
-                if (substr_count($server['server'],':')==1){
-                    list($sftp_server,$sftp_port) = explode(':',$server['server']);
+                if (substr_count($server['server'], ':') == 1) {
+                    list($sftp_server, $sftp_port) = explode(':', $server['server']);
                 }
                 $sftp_username = $server['username'];
                 $sftp_password = $server['password'];
                 $sftp_path = $server['path'];
-        
-        
-                $sftp = new SFTP($sftp_server,$sftp_port);
+
+
+                $sftp = new SFTP($sftp_server, $sftp_port);
                 if (!$sftp->login($sftp_username, $sftp_password)) {
                     throw new \Exception('Login Failed');
                 }
                 $sftp->enableDatePreservation();
                 $sftp->setListOrder('mtime', SORT_ASC);
-        
-        
-                $files = $sftp->nlist($sftp_path.'');
-                foreach($files as $fname){
-                    if (!in_array($fname,['.','..'])){
-        
+
+
+                $files = $sftp->nlist($sftp_path . '');
+                foreach ($files as $fname) {
+                    if (!in_array($fname, ['.', '..'])) {
+
                         $localfname = basename($fname);
-                        $r = $sftp->get( $sftp_path.'/'.$fname , App::get('tempPath') . '/' . $localfname  );
-        
+                        $r = $sftp->get($sftp_path . '/' . $fname, App::get('tempPath') . '/' . $localfname);
+
                         $zip = new \ZipArchive;
-                        if ($zip->open(App::get('tempPath') . '/' . $localfname ) === TRUE) {
+                        if ($zip->open(App::get('tempPath') . '/' . $localfname) === TRUE) {
                             $zip->extractTo(App::get('hlsJobDir'));
                             $zip->close();
                         } else {
                         }
-        
-                        $sftp->delete( $sftp_path.'/'.$fname );
+
+                        $sftp->delete($sftp_path . '/' . $fname);
                     }
                 }
 
-                App::result('msg','Die Daten wurden abgerufen');
-                App::result('success',true);
-            }catch(\Exception $e){
+                App::result('msg', 'Die Daten wurden abgerufen');
+                App::result('success', true);
+            } catch (\Exception $e) {
                 App::result('msg', $e->getMessage());
             }
-        },array('get','post'),true);
-
+        }, array('get', 'post'), true);
     }
 }
--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace Tualo\Office\Whiteboard\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
-class JsLoader implements IRoute{
-    public static function register(){
-        BasicRoute::add('/whiteboard/loader.js',function($matches){
+class JsLoader extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
+        BasicRoute::add('/whiteboard/loader.js', function ($matches) {
             App::contenttype('application/javascript');
             $list = [
                 "js/controller/Portal.js",
@@ -20,12 +23,10 @@ class JsLoader implements IRoute{
                 "js/Routes.js"
             ];
             $content = '';
-            foreach( $list as $item ){
-                $content .= file_get_contents( dirname(__DIR__,1).'/'.$item ).PHP_EOL.PHP_EOL;
+            foreach ($list as $item) {
+                $content .= file_get_contents(dirname(__DIR__, 1) . '/' . $item) . PHP_EOL . PHP_EOL;
             }
-            App::body( $content );
-            
-        },array('get'),false);
-
+            App::body($content);
+        }, array('get'), false);
     }
 }
